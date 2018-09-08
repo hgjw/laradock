26a16a53500c7e6ca484ab725955ab876dc9a47d
黄建威 <huangjianwei2028@163.com>
2018-09-08 18:30:14 +0800

    https证书生成

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -2,5 +2,8 @@
 
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem
+mkdir -p /var/certs/"$CN"
+cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/"$CN"/cert1.pem
+cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/"$CN"/privkey1.pem
+cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/"$CN"/chain1.pem
+cp /etc/letsencrypt/archive/"$CN"/fullchain1.pem /var/certs/"$CN"/fullchain1.pem
